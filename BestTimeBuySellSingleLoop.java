d9f8276204a0fa2fb425328c80c79015edf44709
Ved Chadderwala <96898370+ved-2004@users.noreply.github.com>
2023-04-02 16:37:10 +0530

    Leetcode 75 - Day 5

--- BestTimeBuySellSingleLoop.java
+++ BestTimeBuySellSingleLoop.java
@@ -1,2 +1,18 @@
+/*
+We have two variables, one storing the day with the least price - the day we buy, and the other traversing the array
+At each iteration, we first update the minBuy variable, and then update profit.
+
+This Solution - time beats 82%, mem beats 16%
+Divide and Conquer Approach uploaded as well.
+ */
 public class BestTimeBuySellSingleLoop {
+    public int maxProfit(int[] prices) {
+        int profit = 0, minBuy = Integer.MAX_VALUE;
+        for(int i=0;i<prices.length;i++)
+        {
+            minBuy = Math.min(minBuy,prices[i]);
+            profit = Math.max(profit,prices[i] - minBuy);
+        }
+        return profit;
+    }
 }
--- BestTimeBuySellUsingDNC.java
+++ BestTimeBuySellUsingDNC.java
@@ -1,3 +1,12 @@
+/*
+Using Divide & Conquer - Divide the given array into two equal arrays.
+For a particular array, termination for divide condition when size of array is atmost two.
+For combine - we take min of left array and max of right array and return the difference if >0.
+At the end, we get three profits - one from left, one from right, and one from combine, and return the max of these.
+
+This Solution -  time beats 5%, memory beats 99.2%.
+Single For loop O(n) approach uploaded as well.
+ */
 import java.util.*;
 public class BestTimeBuySellUsingDNC {
     public int combine(int[] arr1, int[] arr2) {
